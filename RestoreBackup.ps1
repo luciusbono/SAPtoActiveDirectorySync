f212d7b9f88d76499d100ed96534f3fec252b22b
Lucius Bono <luciusbono@gmail.com>
2013-11-20 14:40:59 -0800

    Built first run-able script version and RestoreBackup script
    Built first draft script which will sync the following fields:
    Department, Division, extensionAttribute5, title
    
    The script backs up all the users it is about to change before it runs.
    That backup file can be restored via the RestoreBackup script, which is
    functional but basic for the moment.

---
+++ RestoreBackup.ps1
@@ -0,0 +1,7 @@
+﻿$BackupData = Import-Csv c:\users\lbono\Documents\github\SAPtoActiveDirectorySync\backup.csv 
+
+#Revert changes via backup file
+Foreach ($BackupUser in $BackupData)
+{
+	Get-ADUser -Identity $BackupUser.samaccountname | Set-ADUser -Replace @{Department=$BackupUser.Department;Division=$BackupUser.Division;extensionAttribute5=$BackupUser.extensionAttribute5;title=$BackupUser.title}
+}
