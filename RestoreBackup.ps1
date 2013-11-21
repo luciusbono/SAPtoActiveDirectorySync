6dac34d58147854afdb150d8c9e25ee690df9b85
Lucius Bono <luciusbono@gmail.com>
2013-11-20 17:22:29 -0800

    Added parameter input, default backup file output and fixed a bug in the RestoreBackup script (it wasn't importing the CSV!!)
    Still setting the path variables manually for testing purposes, but will
    force the user to provide an input file for the Sync script and a backup
    input file for the RestoreBackup script (a backup file path will be
    displayed automatically when the Sync script is run, although a path can
    be specified)

--- RestoreBackup.ps1
+++ RestoreBackup.ps1
@@ -1,7 +1,12 @@
-﻿$BackupData = Import-Csv c:\users\lbono\Documents\github\SAPtoActiveDirectorySync\backup.csv 
+﻿param($backupPath)
+
+#Remove this after testing complete
+$backupPath = 'c:\users\lbono\Documents\github\SAPtoActiveDirectorySync\backup.csv' 
 
 #Revert changes via backup file
+$BackupData = Import-Csv $backupPath
+
 Foreach ($BackupUser in $BackupData)
 {
-	Get-ADUser -Identity $BackupUser.samaccountname | Set-ADUser -Replace @{Department=$BackupUser.Department;Division=$BackupUser.Division;extensionAttribute5=$BackupUser.extensionAttribute5;title=$BackupUser.title}
+	Get-ADUser -Identity $BackupUser.samaccountname | Set-ADUser -Replace @{Department=$BackupUser.Department;Division=$BackupUser.Division;extensionAttribute5=$BackupUser.extensionAttribute5;title=$BackupUser.title;physicaldeliveryofficename=$BackupUser.office;manager=(Get-ADUser $BackupUser.manager).distinguishedname}
 }
